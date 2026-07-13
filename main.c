8bcacdb779c7754fb1b2834b7af8eecbdc842e28
heklexavier-sys <heklexavier@gmail.com>
2026-07-12 22:08:20 -0500

    Update main.c
    Now can do additon, multiplication, subtraction, division, and all in one function!

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 // /what is an object? something that you can hold in your hand? Something that is induvidual? sometihng with multiople properties? Which
 
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 
@@ -11,7 +12,7 @@
 typedef enum
 {
     OBJ_INT,
-    OBJ_FLOAT,
+    OBJ_DOUBLE,
     OBJ_STRING,
     OBJ_BOOL,
     OBJ_NULL
@@ -24,17 +25,19 @@ typedef struct
     union
     {
         int32_t intValue;
-        float floatValue;
+        double doubleValue;
         const char *stringValue;
         bool boolValue;
     };
 } Value;
+
+
 //DUMMY CONSTANT TABLE
 Value constants[] =
 {
     { .type = OBJ_INT,    .intValue = 5 },
     { .type = OBJ_INT,    .intValue = 10 },
-    { .type = OBJ_FLOAT,  .floatValue = 3.14159f },
+    { .type = OBJ_DOUBLE,  .doubleValue = 3.14159f },
     { .type = OBJ_STRING, .stringValue = "Hello, World!" },
     { .type = OBJ_BOOL,   .boolValue = true },
     { .type = OBJ_BOOL,   .boolValue = false },
@@ -46,6 +49,10 @@ Value constants[] =
 //MY REGISTERS
 Value *registers[256];
 //VERY IMPORTANT
+
+//for determining which register - small int is better
+typedef uint8_t RegIndex;
+
 
 
 typedef enum {
@@ -70,29 +77,122 @@ void loadFunc(int dest, int startIndex, int uselesspeiceofJUNK) {
     registers[dest] = &constants[startIndex];
 }
 
-void addFunc(int dest, int firstNumIndex, int secondNumIndex) {
-    Value *firstNum = registers[firstNumIndex];
-    Value *secondNum = registers[secondNumIndex];
+typedef enum {
+    MATH_ADD,
+    MATH_SUB,
+    MATH_MUL,
+    MATH_DIV,
+} MathType;
+
+void basicMathFunc(RegIndex dest, RegIndex firstNumIndex, RegIndex secondNumIndex, MathType mathType) {
+    
     
 
+    Value *firstNum = registers[firstNumIndex];
+    Value *secondNum = registers[secondNumIndex];
+
+
+    if ((firstNum->type != OBJ_INT &&
+     firstNum->type != OBJ_DOUBLE) ||
+    (secondNum->type != OBJ_INT &&
+     secondNum->type != OBJ_DOUBLE))
+    {
+        printf("Type error\n");
+        return;
+    }
+    
     Value *newValue = malloc(sizeof(Value));
 
-    if (firstNum->type == OBJ_FLOAT || secondNum->type == OBJ_FLOAT) {
-        float result = firstNum->intValue + secondNum->intValue;
-        newValue->type = OBJ_FLOAT;
-        newValue->floatValue = result;
-    } else {
-        int32_t result = firstNum->intValue + secondNum->intValue;
+    if (firstNum->type == OBJ_DOUBLE || secondNum->type == OBJ_DOUBLE)
+    {
+        double holderForFirstNum;
+        double holderForSecondNum;
+
+        if (firstNum->type == OBJ_DOUBLE)
+            holderForFirstNum = firstNum->doubleValue;
+        else
+            holderForFirstNum = (double)firstNum->intValue;
+
+        if (secondNum->type == OBJ_DOUBLE)
+            holderForSecondNum = secondNum->doubleValue;
+        else
+            holderForSecondNum = (double)secondNum->intValue;
+
+        double result;
+        switch(mathType)
+        {
+            case MATH_ADD:
+                result = holderForFirstNum + holderForSecondNum;
+                break;
+
+            case MATH_SUB:
+                result = holderForFirstNum - holderForSecondNum;
+                break;
+
+            case MATH_MUL:
+                result = holderForFirstNum * holderForSecondNum;
+                break;
+
+            case MATH_DIV:
+                if (secondNum->intValue == 0)
+                {
+                    printf("Beep Boop. You failed. Do math better (you divided by zero)\n");
+                    result = 0;
+                } else {
+                    result = holderForFirstNum / holderForSecondNum;
+                }
+                break;
+
+            default:
+                result = 0;
+
+        }
+
+
+        newValue->type = OBJ_DOUBLE;
+        newValue->doubleValue = result;
+    }
+    else
+    {
+        int32_t result;
+        switch(mathType)
+        {
+            case MATH_ADD:
+                result = firstNum->intValue + secondNum->intValue;
+                break;
+
+            case MATH_SUB:
+                result = firstNum->intValue - secondNum->intValue;
+                break;
+
+            case MATH_MUL:
+                result = firstNum->intValue * secondNum->intValue;
+                break;
+
+            case MATH_DIV:
+                if (secondNum->intValue == 0)
+                {
+                    printf("Beep Boop. You failed. Do math better (you divided by zero)\n");
+                    result = 0;
+                } else {
+                    result = firstNum->intValue / secondNum->intValue;
+                }
+                break;
+
+            default:
+                result = 0;
+
+        }
+
         newValue->type = OBJ_INT;
         newValue->intValue = result;
     }
-    
-    registers[dest] = newValue;
 
+    registers[dest] = newValue;
 }
 
 //remember to declare helper functions before they are used
-void doVmStuff(OperationCode opcode, int a, int b, int c)
+void doVmStuff(OperationCode opcode, RegIndex a, RegIndex b, RegIndex c)
 {
     switch(opcode)
     {
@@ -101,7 +201,19 @@ void doVmStuff(OperationCode opcode, int a, int b, int c)
             break;
 
         case OP_ADD:
-            addFunc(a,b,c);
+            basicMathFunc(a, b, c, MATH_ADD);
+            break;
+
+        case OP_SUB:
+            basicMathFunc(a, b, c, MATH_SUB);
+            break;
+
+        case OP_MUL:
+            basicMathFunc(a, b, c, MATH_MUL);
+            break;
+
+        case OP_DIV:
+            basicMathFunc(a, b, c, MATH_DIV);
             break;
     }
 }
