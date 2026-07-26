5f415c2c96589b0b92f26e148f5f2d41cff16fe6
heklexavier-sys <heklexavier@gmail.com>
2026-07-25 21:11:49 -0500

    added logic and bit operations
    also fixed the dub1s and dub2s because they were doing double instead of long double, which is what the bytecode uses.

--- instruction.h
+++ instruction.h
@@ -35,8 +35,8 @@ inline void VMadd(VM* vm) {
     Value src2 = *VMgetNextReg(vm);
 
     if (src1.type == VAL_DOUBLE || src2.type == VAL_DOUBLE) {
-        double dub1 = makeDouble(src1);
-        double dub2 = makeDouble(src2);
+        long double dub1 = makeDouble(src1);
+        long double dub2 = makeDouble(src2);
         dst->type = VAL_DOUBLE;
         dst->data.doubleVal = dub1 + dub2;
     } else {
@@ -51,8 +51,8 @@ inline void VMsub(VM* vm) {
     Value src2 = *VMgetNextReg(vm);
 
     if (src1.type == VAL_DOUBLE || src2.type == VAL_DOUBLE) {
-        double dub1 = makeDouble(src1);
-        double dub2 = makeDouble(src2);
+        long double dub1 = makeDouble(src1);
+        long double dub2 = makeDouble(src2);
         dst->type = VAL_DOUBLE;
         dst->data.doubleVal = dub1 - dub2;
     } else {
@@ -67,8 +67,8 @@ inline void VMmul(VM* vm) {
     Value src2 = *VMgetNextReg(vm);
 
     if (src1.type == VAL_DOUBLE || src2.type == VAL_DOUBLE) {
-        double dub1 = makeDouble(src1);
-        double dub2 = makeDouble(src2);
+        long double dub1 = makeDouble(src1);
+        long double dub2 = makeDouble(src2);
         dst->type = VAL_DOUBLE;
         dst->data.doubleVal = dub1 * dub2;
     } else {
@@ -82,8 +82,8 @@ inline void VMdiv(VM* vm) {
     Value src1 = *VMgetNextReg(vm);
     Value src2 = *VMgetNextReg(vm);
 
-    double dub1 = makeDouble(src1);
-    double dub2 = makeDouble(src2);
+    long double dub1 = makeDouble(src1);
+    long double dub2 = makeDouble(src2);
     dst->type = VAL_DOUBLE;
     dst->data.doubleVal = dub1 / dub2;
 }
@@ -94,8 +94,8 @@ inline void VMmod(VM* vm) {
     Value src2 = *VMgetNextReg(vm);
 
     if (src1.type == VAL_DOUBLE || src2.type == VAL_DOUBLE) {
-        double dub1 = makeDouble(src1);
-        double dub2 = makeDouble(src2);
+        long double dub1 = makeDouble(src1);
+        long double dub2 = makeDouble(src2);
         dst->type = VAL_DOUBLE;
         dst->data.doubleVal = fmod(dub1, dub2);
     } else {
@@ -116,6 +116,210 @@ inline void VMneg(VM* vm) {
         dst->data.intVal = -src.data.intVal;
     }
 }
+
+inline void VMbitand(VM* vm) {
+    Value* dst = VMgetNextReg(vm);
+    Value src1 = *VMgetNextReg(vm);
+    Value src2 = *VMgetNextReg(vm);
+
+    dst->type = VAL_INT;
+    dst->data.intVal = src1.data.intVal & src2.data.intVal;
+}
+
+inline void VMbitor(VM* vm) {
+    Value* dst = VMgetNextReg(vm);
+    Value src1 = *VMgetNextReg(vm);
+    Value src2 = *VMgetNextReg(vm);
+
+    dst->type = VAL_INT;
+    dst->data.intVal = src1.data.intVal | src2.data.intVal;
+}
+
+inline void VMbitxor(VM* vm) {
+    Value* dst = VMgetNextReg(vm);
+    Value src1 = *VMgetNextReg(vm);
+    Value src2 = *VMgetNextReg(vm);
+
+    dst->type = VAL_INT;
+    dst->data.intVal = src1.data.intVal ^ src2.data.intVal;
+}
+
+inline void VMbitnot(VM* vm) {
+    Value* dst = VMgetNextReg(vm);
+    Value src = *VMgetNextReg(vm);
+
+    dst->type = VAL_INT;
+    dst->data.intVal = ~(src.data.intVal);
+}
+
+inline void VMbitshl(VM* vm) {
+    Value* dst = VMgetNextReg(vm);
+    Value src1 = *VMgetNextReg(vm);
+    Value src2 = *VMgetNextReg(vm);
+
+    dst->type = VAL_INT;
+    dst->data.intVal = src1.data.intVal << src2.data.intVal;
+}
+
+inline void VMbitshr(VM* vm) {
+    Value* dst = VMgetNextReg(vm);
+    Value src1 = *VMgetNextReg(vm);
+    Value src2 = *VMgetNextReg(vm);
+
+    dst->type = VAL_INT;
+    dst->data.intVal = src1.data.intVal >> src2.data.intVal;
+}
+
+inline void VMeq(VM* vm) {
+    Value* dst = VMgetNextReg(vm);
+    Value src1 = *VMgetNextReg(vm);
+    Value src2 = *VMgetNextReg(vm);
+
+    dst->type = VAL_BOOL;
+
+    if (src1.type != src2.type) {
+        //should i even bother with a catch for this?
+        dst->data.boolVal = false;
+        return;
+    }
+
+    switch (src1.type) {
+        case VAL_INT:
+            dst->data.boolVal =
+                src1.data.intVal == src2.data.intVal;
+            break;
+
+        case VAL_DOUBLE:
+            dst->data.boolVal =
+                src1.data.doubleVal == src2.data.doubleVal;
+            break;
+
+        case VAL_BOOL:
+            dst->data.boolVal =
+                src1.data.boolVal == src2.data.boolVal;
+            break;
+
+        case VAL_STRING:
+            dst->data.stringVal =
+                src1.data.stringVal == src2.data.stringVal;
+            break;
+
+            //should i do one for objects too?
+        default:
+            dst->data.boolVal = false;
+            break;
+    }
+}
+
+inline void VMne(VM* vm) {
+    Value* dst = VMgetNextReg(vm);
+    Value src1 = *VMgetNextReg(vm);
+    Value src2 = *VMgetNextReg(vm);
+
+    dst->type = VAL_BOOL;
+
+    if (src1.type != src2.type) {
+        //should i even bother with a catch for this?
+        dst->data.boolVal = false;
+        return;
+    }
+
+    switch (src1.type) {
+        case VAL_INT:
+            dst->data.boolVal =
+                src1.data.intVal != src2.data.intVal;
+            break;
+
+        case VAL_DOUBLE:
+            dst->data.boolVal =
+                src1.data.doubleVal != src2.data.doubleVal;
+            break;
+
+        case VAL_BOOL:
+            dst->data.boolVal =
+                src1.data.boolVal != src2.data.boolVal;
+            break;
+
+        case VAL_STRING:
+            dst->data.stringVal =
+                src1.data.stringVal != src2.data.stringVal;
+            break;
+
+        default:
+            dst->data.boolVal = false;
+            break;
+    }
+}
+
+inline void VMlt(VM* vm) {
+    Value* dst = VMgetNextReg(vm);
+    Value src1 = *VMgetNextReg(vm);
+    Value src2 = *VMgetNextReg(vm);
+
+    dst->type = VAL_BOOL;
+
+    if (src1.type == VAL_DOUBLE || src2.type == VAL_DOUBLE) {
+        long double dub1 = makeDouble(src1);
+        long double dub2 = makeDouble(src2);
+        dst->data.boolVal = (dub1 < dub2);
+    } else {
+
+        dst->data.boolVal = (src1.data.intVal < src2.data.intVal);
+    }
+}
+
+inline void VMle(VM* vm) {
+    Value* dst = VMgetNextReg(vm);
+    Value src1 = *VMgetNextReg(vm);
+    Value src2 = *VMgetNextReg(vm);
+
+    dst->type = VAL_BOOL;
+
+    if (src1.type == VAL_DOUBLE || src2.type == VAL_DOUBLE) {
+        long double dub1 = makeDouble(src1);
+        long double dub2 = makeDouble(src2);
+        dst->data.boolVal = (dub1 <= dub2);
+    } else {
+
+        dst->data.boolVal = (src1.data.intVal <= src2.data.intVal);
+    }
+}
+
+inline void VMgt(VM* vm) {
+    Value* dst = VMgetNextReg(vm);
+    Value src1 = *VMgetNextReg(vm);
+    Value src2 = *VMgetNextReg(vm);
+
+    dst->type = VAL_BOOL;
+
+    if (src1.type == VAL_DOUBLE || src2.type == VAL_DOUBLE) {
+        long double dub1 = makeDouble(src1);
+        long double dub2 = makeDouble(src2);
+        dst->data.boolVal = (dub1 > dub2);
+    } else {
+
+        dst->data.boolVal = (src1.data.intVal > src2.data.intVal);
+    }
+}
+
+inline void VMge(VM* vm) {
+    Value* dst = VMgetNextReg(vm);
+    Value src1 = *VMgetNextReg(vm);
+    Value src2 = *VMgetNextReg(vm);
+
+    dst->type = VAL_BOOL;
+
+    if (src1.type == VAL_DOUBLE || src2.type == VAL_DOUBLE) {
+        long double dub1 = makeDouble(src1);
+        long double dub2 = makeDouble(src2);
+        dst->data.boolVal = (dub1 >= dub2);
+    } else {
+
+        dst->data.boolVal = (src1.data.intVal >= src2.data.intVal);
+    }
+}
+
+
 
 
 #endif
